03add7914a6dd484acef2ce275e7c22efb8fc686
DangMinhHuy308 <huyminhdang5@gmail.com>
2024-03-18 20:47:48 +0700

    Update Board Content v2

--- src/pages/Boards/BoardBar/BoadBar.jsx
+++ src/pages/Boards/BoardBar/BoadBar.jsx
@@ -37,7 +37,7 @@ function BoardBar() {
       gap: 2,
       overflowX: 'auto',
       bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#33495e': '#1976d2 '),
-      borderBottom: '1px solid white'
+      
 
     }}>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
