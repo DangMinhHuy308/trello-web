2f65b7269733fb873e458be33abecb975c647abb
DangMinhHuy308 <huyminhdang5@gmail.com>
2024-03-26 16:21:06 +0700

    Deploy v3

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,6 +5,6 @@ if (process.env.BUILD_MODE === 'dev') {
 if (process.env.BUILD_MODE === 'production') {
   apiRoot = 'https://trello-api-y0pj.onrender.com'
 }
-console.log('apiRoot',apiRoot);
+// console.log('apiRoot',apiRoot);
 export const API_ROOT = apiRoot
 // export const API_ROOT = 'https://trello-api-y0pj.onrender.com'
